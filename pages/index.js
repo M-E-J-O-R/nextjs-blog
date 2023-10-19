dc08b0361fdd7420674993370a03537776001ea4
Mejor <amujobabatunde7@gmail.com>
2023-10-19 09:51:30 +0100

    learnt about using Link component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 const Home = () => {
     return (
 
-        <h1>Home Page</h1>
+        <>
+            <Link href="/about">
+                <a >about
+                </a>
+            </Link>
+            <br />
+            <Link href="contact">
+                <a >contact
+                </a>
+            </Link>
+            <br />
+            <Link href="doc">
+                <a >doc
+                </a>
+            </Link>
+            <br />
+            <Link href="blog">
+                <a >blog
+                </a>
+            </Link>
+            <h1>Home Page</h1>
+        </>
+
 
     );
 };
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,10 +1,23 @@
+import Link from "next/link";
+
 const Product = () => {
     return (
         <dvi>
+            <Link href="/">
+                <a > Home</a>
+            </Link>
+
             <h1>Product Page</h1>
-            <h2>Product one</h2>
-            <h2>Product two</h2>
-            <h2>Product three</h2>
+            <Link href='/product/1'>
+                <a > Product 1</a>
+            </Link>
+            <Link href='/product/2'>
+                <a > Product 2</a>
+            </Link>
+            <Link href='/product/3'>
+                <a > Product 3</a>
+            </Link>
+
         </dvi>);
 };
 
