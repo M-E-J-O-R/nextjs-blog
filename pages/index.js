038c030857508fc87766ab44146545c5a8bd6028
Mejor <amujobabatunde7@gmail.com>
2023-10-19 11:20:51 +0100

    learnt how to programmatically route

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Home = () => {
+
+    const router = useRouter();
+    const handleRouting = () => {
+        console.log('This is a programmatic routing');
+        router.push('/product');
+    };
     return (
 
         <>
@@ -24,6 +31,8 @@ const Home = () => {
                 </a>
             </Link>
             <h1>Home Page</h1>
+            <button onClick={handleRouting}> Click me and go to product</button>
+
         </>
 
 
