ef7e20b3089eb0bd57ed53a65bac882749ed02d0
Mejor <amujobabatunde7@gmail.com>
2023-10-23 00:22:52 +0100

    learnt about incremental static regeneration

--- pages/product/[productId]/index.js
+++ pages/product/[productId]/index.js
@@ -1,11 +1,44 @@
 import { useRouter } from "next/router";
 
-const ProductDetails = () => {
-    const router = useRouter();
-    const productId = router.query.productId;
+const Products = ({ product }) => {
+    const router = useRouter()
+    
+    if (router.isFallback) {
+        return <div>Loading....</div>
+    }
     return (
-        <h2>Details of the product selected {productId}</h2>
+        <div>
+            <h1>{product.id} {product.name}</h1>
+            <h2>{product.body}</h2>
+            <h2>{product.price}</h2>
+        </div>
     );
 };
 
-export default ProductDetails;
+export default Products;
+
+export const getStaticProps = async (context) => {
+    const { params } = context;
+    const response = await fetch(`http://localhost:3001/product/${params.productId}`);
+    const data = await response.json();
+    console.log('Regenerating data')
+
+    return {
+        props: {
+            product: data
+        },
+        revalidate: 15,
+    };
+
+};
+
+
+
+export const getStaticPaths = async () => {
+    return {
+        paths: [
+            { params: { productId: '1' } }
+        ],
+        fallback: true
+    };
+};
--- pages/product/index.js
+++ pages/product/index.js
@@ -8,13 +8,13 @@ const Product = () => {
             </Link>
 
             <h1>Product Page</h1>
-            <Link href='/product/1'>
+            <Link href='product/1'>
                 <a > Product 1</a>
             </Link>
-            <Link href='/product/2'>
+            <Link href='product/2'>
                 <a > Product 2</a>
             </Link>
-            <Link href='/product/3'>
+            <Link href='product/3'>
                 <a > Product 3</a>
             </Link>
 
@@ -22,3 +22,16 @@ const Product = () => {
 };
 
 export default Product;
+
+export const getStaticProps = async () => {
+
+    const response = await fetch('http://localhost:3001/product');
+    const data = await response.json();
+    console.log(data)
+
+    return {
+        props: {
+            product: data
+        }
+    };
+};
