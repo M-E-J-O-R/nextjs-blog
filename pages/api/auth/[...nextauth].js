922de974ead6e2cb266c62c01df9d32137db559e
Mejor <amujobabatunde7@gmail.com>
2023-11-11 21:57:26 +0100

    restructure the code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
 
 export default NextAuth({
     providers: [
@@ -7,5 +7,26 @@ export default NextAuth({
             clientId: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECRET
         })
-    ]
-})
+    ],
+    database: process.env.DB_URL,
+    session: {
+        jwt: true,
+    },
+    jwt: {
+        secret: 'dlaodaohle ahohoho'
+    },
+    callbacks: {
+        async jwt(token, user) {
+            console.log(user)
+            if (user && user.id) {
+                token.id = user.id;
+            }
+            return token;
+        },
+        async session(session, token) {
+            session.user.id = token.id;
+            return session
+        }
+    }
+
+});
